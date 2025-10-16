5dab375320278d5490b88da66f512f2b706d0da9
seven - Erivan S Cerqueira <7b37b3@gmail.com>
2025-10-16 18:40:50 -0300

    first commit

---
+++ init.lua
@@ -0,0 +1,20 @@
+require("config.lazy")
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+
+local opt = vim.opt
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- Highlight when yanking (copying) text
+-- Try it with `yap` normal mode
+-- See `:help vim.highlight.on_yank()`
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking ( copying ) text',
+	group = vim.api.nvim_create_augroup('anyword', { clear = true }),
+	callback = function()
+	vim.highlight.on_yank()
+end,
+
+})
