ae2f576dd7ad3b3d5002e8108794fdc78b113676
seven - Erivan S Cerqueira <7b37b3@gmail.com>
2025-10-18 11:22:48 -0300

    lsp emmet update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -148,29 +148,15 @@ return {
             },
         })
 
-        -- LSP servers and clients are able to communicate to each other what features they support.
-        --  By default, Neovim doesn't support everything that is in the LSP specification.
-        --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-        --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
         local capabilities = vim.lsp.protocol.make_client_capabilities()
-        -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-        -- Enable the following language servers
-        --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-        --
-        --  Add any additional override configuration in the following tables. Available keys are:
-        --  - cmd (table): Override the default command used to start the server
-        --  - filetypes (table): Override the default list of associated filetypes for the server
-        --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-        --  - settings (table): Override the default settings passed when initializing the server.
-        --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
             bashls = {},
             marksman = {},
             tailwindcss = {},
-            emmet_ls = {
-                filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-            },
+            -- emmet_ls = {
+            -- filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+            -- },
+
             svelte = {},
             -- clangd = {},
             -- gopls = {},
@@ -190,16 +176,16 @@ return {
                 -- filetypes = { ... },
                 -- capabilities = {},
                 settings = {
-                  Lua = {
-                    completion = {
-                      callSnippet = 'Replace',
+                    Lua = {
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                        -- diagnostics = { disable = { 'missing-fields' } },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
                     },
-                    -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                    -- diagnostics = { disable = { 'missing-fields' } },
-                     workspace = {
-                       library = vim.api.nvim_get_runtime_file("", true),
-                     },
-                  },
                 },
             },
         }
