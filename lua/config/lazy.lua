a0669f02abbbe2317bce5b04726ce6b2dea1051f
seven - esc <7b37b3@gmail.com>
2025-11-01 21:01:37 -0300

    add precognition and change the C-w for leader-w

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,3 @@ require("lazy").setup({
     -- checker = { enabled = true },
 })
 require("config.keymaps")
-
--- For video recording purposes
-vim.cmd("ShowkeysToggle")
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -31,7 +31,7 @@ return {
                 end,
                 max_name_length = 18,
                 max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-                truncate_names = true, -- whether or not tab names should be truncated
+                truncate_names = true,  -- whether or not tab names should be truncated
                 tab_size = 18,
                 diagnostics = "nvim_lsp",
                 diagnostics_update_in_insert = false,
@@ -67,17 +67,18 @@ return {
                         separator = true,
                     },
                 },
-                color_icons = true, -- whether or not to add the filetype icon highlights
+                color_icons = true,       -- whether or not to add the filetype icon highlights
                 show_buffer_icons = true, -- disable filetype icons for buffers
                 show_buffer_close_icons = true,
                 get_element_icon = function(element)
-                    local icon, hl = require('nvim-web-devicons').get_icon(element.name, element.path, { default = false })
+                    local icon, hl = require('nvim-web-devicons').get_icon(element.name, element.path,
+                        { default = false })
                     return icon, hl
                 end,
                 show_close_icon = true,
                 show_tab_indicators = true,
                 show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
-                persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+                persist_buffer_sort = true,   -- whether or not custom sorted buffers should persist
                 -- can also be a table containing 2 custom separators
                 -- [focused and unfocused]. eg: { '|', '|' }
                 separator_style = "slant", -- | "slope" | "thick" | "thin" | { 'any', 'any' },
@@ -97,6 +98,7 @@ return {
         -- Keymaps for buffer navigation
         vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous buffer" })
         vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
-        vim.keymap.set("n", "<S-w>", "<cmd>bdelete!<cr>", { desc = "Close current buffer" })
+        vim.keymap.set("n", "<leader>w", "<cmd>bdelete!<cr>", { desc = "Close current buffer" })
     end,
 }
+
