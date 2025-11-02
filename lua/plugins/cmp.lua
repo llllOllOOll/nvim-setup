b0ac26d25dc9408a9681ffa010510c31f2edfb53
seven - esc <7b37b3@gmail.com>
2025-11-01 22:28:15 -0300

    fix auto complete

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,98 +1,73 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-      "hrsh7th/cmp-emoji",
-      "ray-x/cmp-sql",
-    },
-    config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
 
-      require('luasnip.loaders.from_vscode').lazy_load()
+			-- Load VSCode-style snippets
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-      -- Extend svelte filetype to include html snippets
-      local ls = require("luasnip")
-      ls.filetype_extend("svelte", { "html" })
+			-- Extend svelte to include html snippets
+			luasnip.filetype_extend("svelte", { "html" })
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<C-j>'] = cmp.mapping.select_next_item(),
-          ['<C-k>'] = cmp.mapping.select_prev_item(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-y>'] = cmp.mapping.abort(),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            elseif cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp', priority = 80 },
-          { name = 'lazydev', priority = 100 },
-          { name = 'luasnip', priority = 90 },
-          { name = 'buffer', priority = 40 },
-          { name = 'path', priority = 20 },
-          {
-            name = 'emoji',
-            priority = 10,
-            option = { insert = true },
-            enabled = function()
-              return vim.tbl_contains({ "gitcommit", "markdown" }, vim.o.filetype)
-            end,
-          },
-          {
-            name = 'sql',
-            priority = 10,
-            enabled = function()
-              return vim.tbl_contains({ "sql" }, vim.o.filetype)
-            end,
-          },
-        }),
-        completion = { completeopt = 'menu,menuone,noinsert' },
-      })
-
-      cmp.setup.cmdline('/', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
-
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' },
-          { name = 'cmdline' }
-        })
-      })
-    end,
-  },
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol_text",
+						maxwidth = 50,
+					}),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						elseif cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				},
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+			})
+		end,
+	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,103 +1,105 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        { "williamboman/mason.nvim", config = true },
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        { "j-hui/fidget.nvim",       opts = {} },
-        "ibhagwan/fzf-lua", -- keep FZF integration
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason.nvim", config = true },
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{ "j-hui/fidget.nvim", opts = {} },
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local mason_lsp = require("mason-lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    config = function()
-        local lspconfig = require("lspconfig")
-        local mason_lsp = require("mason-lspconfig")
+		-- LSP keymaps
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(event)
+				local fzf = require("fzf-lua")
+				local map = function(keys, func, desc, mode)
+					mode = mode or "n"
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				end
 
-        -- Capabilities (blink or cmp)
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+				map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+				map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+				map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+				map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+				map("<leader>ds", fzf.lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("<leader>ws", fzf.lsp_live_workspace_symbols, "[W]orkspace [S]ymbols")
+				map("<leader>cr", vim.lsp.buf.rename, "[C]ode [R]ename")
+				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+				map("K", vim.lsp.buf.hover, "Hover Documentation")
+			end,
+		})
 
-        -- LSP keymaps (with FZF)
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(event)
-                local fzf = require("fzf-lua")
-                local map = function(keys, func, desc, mode)
-                    vim.keymap.set(mode or "n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-                end
+		-- Diagnostic configuration
+		vim.diagnostic.config({
+			severity_sort = true,
+			float = { border = "rounded" },
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = " ",
+					[vim.diagnostic.severity.HINT] = " ",
+				},
+			},
+		})
 
-                map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-                map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
-                map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-                map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-                map("<leader>ds", fzf.lsp_document_symbols, "[D]ocument [S]ymbols")
-                map("<leader>ws", fzf.lsp_live_workspace_symbols, "[W]orkspace [S]ymbols")
-                map("<leader>cr", vim.lsp.buf.rename, "[R]e[n]ame")
-                map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-                map("K", vim.lsp.buf.hover, "Hover Docs")
-            end,
-        })
+		-- LSP server configurations
+		local servers = {
+			lua_ls = {
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+						workspace = { checkThirdParty = false },
+					},
+				},
+			},
+			ts_ls = {},
+			svelte = {
+				settings = {
+					svelte = {
+						completions = { emmet = false },
+					},
+				},
+			},
+			tailwindcss = {},
+			bashls = {},
+			marksman = {},
+			cssls = {},
+			kotlin_language_server = {
+				settings = {
+					kotlin = {
+						compiler = {
+							jvm = {
+								target = "21",
+							},
+						},
+					},
+				},
+			},
+			zls = {},
+		}
 
-        -- Diagnostic settings
-        vim.diagnostic.config({
-            severity_sort = true,
-            float = { border = "rounded" },
-            signs = {
-                text = {
-                    [vim.diagnostic.severity.ERROR] = "󰅚 ",
-                    [vim.diagnostic.severity.WARN] = "󰀪 ",
-                    [vim.diagnostic.severity.INFO] = "󰋽 ",
-                    [vim.diagnostic.severity.HINT] = "󰌶 ",
-                },
-            },
-        })
+		-- Ensure tools are installed
+		local ensure = vim.tbl_keys(servers)
+		vim.list_extend(ensure, { "stylua", "prettier" })
 
-        -- LSP servers configuration
-        local servers = {
-            lua_ls = {
-                settings = {
-                    Lua = {
-                        diagnostics = { globals = { "vim" } },
-                        workspace = { checkThirdParty = false },
-                    },
-                },
-            },
-            ts_ls = {},
-            svelte = {
-                settings = {
-                    svelte = {
-                        completions = { emmet = false },
-                    },
-                },
-            },
-            tailwindcss = {},
-            bashls = {},
-            marksman = {},
-            cssls = {},
-            kotlin_language_server = {
-                settings = {
-                    kotlin = {
-                        compiler = {
-                            jvm = {
-                                target = "21"
-                            }
-                        }
-                    }
-                }
-            },
-        }
+		require("mason-tool-installer").setup({
+			ensure_installed = ensure,
+		})
 
-        -- Mason ensure installation
-        local ensure = vim.tbl_keys(servers)
-        vim.list_extend(ensure, { "stylua", "prettier" })
-        require("mason-tool-installer").setup({ ensure_installed = ensure })
-
-        mason_lsp.setup({
-            automatic_installation = true,
-            handlers = {
-                function(server)
-                    local opts = servers[server] or {}
-                    opts.capabilities = vim.tbl_deep_extend("force", {}, capabilities, opts.capabilities or {})
-                    lspconfig[server].setup(opts)
-                end,
-            },
-        })
-    end,
+		-- Setup LSP servers
+		mason_lsp.setup({
+			automatic_installation = true,
+			handlers = {
+				function(server)
+					local opts = servers[server] or {}
+					opts.capabilities = vim.tbl_deep_extend("force", {}, capabilities, opts.capabilities or {})
+					lspconfig[server].setup(opts)
+				end,
+			},
+		})
+	end,
 }
