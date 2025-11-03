bb2fd591272056ce6abb5a00be75c0cca326e1ea
seven - esc <7b37b3@gmail.com>
2025-11-03 18:16:46 -0300

    change close buffer keybind to x

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -98,6 +98,6 @@ return {
 		-- Keymaps for buffer navigation
 		vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous buffer" })
 		vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
-		vim.keymap.set("n", "<S-t>", "<cmd>bdelete!<cr>", { desc = "Close current buffer" })
+		vim.keymap.set("n", "<S-x>", "<cmd>bdelete!<cr>", { desc = "Close current buffer" })
 	end,
 }
