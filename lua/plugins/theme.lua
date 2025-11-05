40723524a15367de2468e2aeda7c2b77ef015992
seven - esc <7b37b3@gmail.com>
2025-11-05 09:45:54 -0300

    change the theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,20 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-        })
-        vim.cmd("colorscheme catppuccin")
-    end,
+	"navarasu/onedark.nvim",
+	priority = 1000,
+	config = function()
+		require("onedark").setup({
+			-- Choose a style variant
+			style = "warm", -- 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
+		})
+		vim.cmd("colorscheme onedark")
+	end,
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
+	-- priority = 1000,
+	-- config = function()
+	--     require("catppuccin").setup({
+	--         flavour = "mocha",
+	--     })
+	--     vim.cmd("colorscheme catppuccin")
+	-- end,
 }
